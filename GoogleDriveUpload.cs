411dffecf076a8388b9d82b171453dde8f9d7f2f
rajibsahani29 <rajibsahani29@gmail.com>
2022-02-01 15:20:32 +0530

    Add files via upload

---
+++ GoogleDriveUpload.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Google.Apis.Drive.v2;
+using Google.Apis.Auth.OAuth2;
+using System.Threading;
+using Google.Apis.Util.Store;
+using Google.Apis.Services;
+using Google.Apis.Drive.v2.Data;
+using System.Collections.Generic;
+
+namespace GoogleDriveUploadLibrary
+{
+    public class GoogleDriveUpload
+    {
+        //// 
+
+        /// Create a new Directory.
+        /// Documentation: https://developers.google.com/drive/v2/reference/files/insert
+        /// 
+
+        /// a Valid authenticated DriveService
+        /// The title of the file. Used to identify file or folder name.
+        /// A short description of the file.
+        /// Collection of parent folders which contain this file. 
+        ///                       Setting this field will put the file in all of the provided folders. root folder.
+        /// 
+        public static File createDirectory(DriveService _service, string _title, string _description, string _parent)
+        {
+
+            File NewDirectory = null;
+
+            // Create metaData for a new Directory
+            File body = new File();
+            body.Title = _title;
+            body.Description = _description;
+            body.MimeType = "application/vnd.google-apps.folder";
+
+            //body.Parents = new List() { new ParentReference() { Id = _parent } };
+            try
+            {
+                FilesResource.InsertRequest request = _service.Files.Insert(body);
+                NewDirectory = request.Execute();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+
+            return NewDirectory;
+        }
+
+        // tries to figure out the mime type of the file.
+        private static string GetMimeType(string fileName)
+        {
+            string mimeType = "application/unknown";
+            string ext = System.IO.Path.GetExtension(fileName).ToLower();
+            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
+            if (regKey != null && regKey.GetValue("Content Type") != null)
+                mimeType = regKey.GetValue("Content Type").ToString();
+            return mimeType;
+        }
+
+        /// 
+
+        /// Uploads a file
+        /// Documentation: https://developers.google.com/drive/v2/reference/files/insert
+        /// 
+
+        /// a Valid authenticated DriveService
+        /// path to the file to upload
+        /// Collection of parent folders which contain this file. 
+        ///                       Setting this field will put the file in all of the provided folders. root folder.
+        /// If upload succeeded returns the File resource of the uploaded file 
+        ///          If the upload fails returns null
+        public File uploadFile(DriveService _service, byte[] byteArray, string _parent, string Description, Dictionary<string, object> metadataValues, string FileName)
+        {
+            File body = new File();
+            body.Title = FileName;
+            body.Description = Description;
+            body.MimeType = GetMimeType(FileName);
+
+            // Set the parent folder.
+            if (!String.IsNullOrEmpty(_parent))
+            {
+                body.Parents = new List<ParentReference>()
+                             {new ParentReference() {Id = _parent}};
+            }
+
+            foreach (var item in metadataValues)
+            {
+                Property newProperty = new Property();
+                newProperty.Key = item.Key;
+                newProperty.Value = item.Value.ToString();
+                //newProperty.Visibility = "PUBLIC";
+
+                _service.Properties.Insert(newProperty, _parent).Execute();
+            }
+
+            // File's content.
+            System.IO.MemoryStream stream = new System.IO.MemoryStream(byteArray);
+            try
+            {
+                FilesResource.InsertMediaUpload request = _service.Files.Insert(body, stream, GetMimeType(FileName));
+                request.Upload();
+                File file = request.ResponseBody;
+                return request.ResponseBody;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+                return null;
+            }
+        }
+    }
+}
